5a82771bb2797f2ab5d06839bb92092b040150bc
Nikolaj <nikolaj.schlumbohm@gmail.com>
2025-08-22 14:39:34 +0200

    some view duration analysis progress

--- view_duration_analyzer.py
+++ view_duration_analyzer.py
@@ -2,6 +2,7 @@
 # measures view duration for stimuli and participants
 # analyzes view duration patterns for grouped data
 import pandas as pd
+import matplotlib.pyplot as plt
 
 view_duration_df = pd.read_csv('view_durations.csv')
 
@@ -24,3 +25,16 @@ print(grouped_by_image.head())
 
 #--Group by participant_id
 #--Group by tag
+
+#plot system_time_diff_seconds with different lines for each participant for all images by increasing id on the x-axis
+
+for participant_id in range(1,10):
+    participant_data = view_duration_df[view_duration_df['participant_id'] == participant_id]
+    print(f"Participant {participant_id} Data:\n", participant_data)
+    plt.plot(participant_data['image_name'], participant_data['system_time_diff_seconds'], label=f'Participant {participant_id}')
+    
+plt.xlabel('Image Name')
+plt.ylabel('System Time Diff (seconds)')
+plt.title(f'System Time Diff for Participant {participant_id}')
+plt.xticks(rotation=90)
+plt.show()
--- view_duration_calculator.py
+++ view_duration_calculator.py
@@ -8,19 +8,26 @@ durations_df = pd.DataFrame(columns=['participant_id', 'file_name', 'image_name'
 for file in os.listdir('.'):
     if file.startswith("Proband") and not "Valididitätskontrolle" in file and file.endswith('.csv'):
         current_frame = pd.read_csv(file, sep=',')
+
         device_start_time = current_frame['device_time_stamp'].iloc[0]
         device_end_time = current_frame['device_time_stamp'].iloc[-1]
         device_time_diff = device_end_time - device_start_time
+
         system_start_time = current_frame['system_time_stamp'].iloc[0]
         system_end_time = current_frame['system_time_stamp'].iloc[-1]
         system_time_diff = system_end_time - system_start_time
+
         current_times = {
-            'participant_id': [file.strip("Proband").split('_')[0]], 
+            # participant_id is only the number of the participant 
+            'participant_id': [file.strip("Proband").split('_')[0]],
+            #file name is the full file name
             'file_name': [file],
             # image_name is the file name from id... onwards
             #e.g. Proband53_15_41_08_id088_politik -> id088_politik
             'image_name': [file[(file.index("id")):].strip('.csv')+".jpg"],
+            # length of time between first and last system time stamp in seconds
             'system_time_diff_seconds': [system_time_diff/1000000],
+            # length of time between first and last device time stamp in seconds
             'device_time_diff_seconds': [device_time_diff/1000000]}
         current_times_df = pd.DataFrame(current_times)
         durations_df = pd.concat([durations_df,current_times_df], ignore_index=True)
